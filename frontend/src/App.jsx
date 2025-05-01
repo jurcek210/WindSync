f5e8b33ca86e486395c40b041acd9261afa0dbb0
MaticTheCoder <maticckorenn@gmail.com>
2025-05-01 23:00:06 +0200

    drugo ozadje ker me bolijo oke pa base za homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register"
+import Home from "./components/Home"
+import "./styles/index.css"
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/home" element ={<Home/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
