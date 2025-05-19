59958fb93faeb5109c5b0f55eb3ed6dd471f0f8c
jurcek210 <159778403+jurcek210@users.noreply.github.com>
2025-05-19 21:01:07 +0200

    WIN-15

---
+++ DSL/DSLmreza/src/Symbol.kt
@@ -0,0 +1,35 @@
+enum class Symbol {
+    EOF,
+    VARIABLE,
+    STRING,
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDES,
+    GET,
+    POINT,
+    SET,
+    TRUE,
+    FALSE,
+    MARKER,
+    ZELENAMREZA,
+    LINE,
+    BEND,
+    LPAREN,
+    RPAREN,
+    EQUALS,
+    BOX,
+    CIRCLE,
+    CONNECT,
+    AREA,
+    COUNT,
+    LINK, //to je za sladkorček --
+    REAL,
+    ASSIGN,
+    BEGIN,// {
+    END, // }
+    COLON,
+    SEMICOLON,
+    TO
+
+}
